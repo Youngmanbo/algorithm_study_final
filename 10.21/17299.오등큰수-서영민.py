6a3dc7a0daac07fdcdbaa95151647cf5fc005e96
Youngmanbo <yo900326@gmail.com>
2023-10-22 00:38:42 +0900

    17299 풀이 수정

--- 10.21/17299.오등큰수-서영민.py
+++ 10.21/17299.오등큰수-서영민.py
@@ -11,7 +11,22 @@ for num in arr:
     count_arr[num] += 1
 
 stack = []
-other_stack = []
 
 stack.append(arr[-1])
 arr[-1] = -1
+
+for i in range(N-2, -1, -1):
+    flag = True
+    while stack:
+        if count_arr[arr[i]] < count_arr[stack[-1]]:
+            temp = stack[-1]
+            stack.append(arr[i])
+            arr[i] = temp
+            flag = False
+            break
+        else:
+            stack.pop()
+    if flag:
+        stack.append(arr[i])
+        arr[i] = -1
+print(*arr)
